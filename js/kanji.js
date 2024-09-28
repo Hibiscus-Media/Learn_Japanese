5455f6666e98ef153607a67d96f8b617207acca3
Hibiscus-Media <hibiscusmedia1@gmail.com>
2024-09-28 13:11:10 +1000

    Updated index.html and kanji.html with new images and dynamic JS

--- js/kanji.js
+++ js/kanji.js
@@ -1,27 +1,26 @@
-// Object to hold the folder names and images dynamically
 const folderImages = {
-    Days_Of_Week: ['1-Monday.jpeg', '2-Tuesday.jpeg', '3-Wednesday.jpeg', '4-Thursday.jpeg', '5-Friday.jpeg', '6-Saturday.jpeg', '7-Sunday.jpeg']
-    // Add more folders and images here if needed
+    'Days_Of_Week': [
+        '*.jpg',
+        '1-Monday.jpeg',
+        '2-Tuesday.jpeg',
+        '3-Wednesday.jpeg',
+        '4-Thusday.jpeg',
+        '5-Friday.jpeg',
+        '6-Saturday.jpeg',
+        '7-Sunday.jpeg',
+        'Banner.jpeg',
+    ],
 };
-
-// Reference to HTML elements
 const contentsList = document.getElementById('contents-list');
 const kanjiSections = document.getElementById('kanji-sections');
-
-// Loop through each folder and generate its section and images
 Object.keys(folderImages).forEach(folder => {
-    // Add a link to the content list
     const listItem = document.createElement('li');
     listItem.innerHTML = `<a href='#${folder}'>${folder.replace(/_/g, ' ')}</a>`;
     contentsList.appendChild(listItem);
-
-    // Create a section for each folder
     const section = document.createElement('div');
     section.id = folder;
     section.innerHTML = `<h2>${folder.replace(/_/g, ' ')}</h2><div class='gallery' data-aos='fade-up'></div>`;
     kanjiSections.appendChild(section);
-
-    // Add images to the section
     folderImages[folder].forEach(image => {
         const img = document.createElement('img');
         img.src = `Kanji/${folder}/${image}`;
@@ -29,6 +28,4 @@ Object.keys(folderImages).forEach(folder => {
         section.querySelector('.gallery').appendChild(img);
     });
 });
-
-// Initialize AOS (Animate on Scroll) for gallery animations
 AOS.init();
